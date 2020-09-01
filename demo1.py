667995f267eeac0d0f54083b62816d06e91c7acc
hapetrosyan <hapetrosyan@gmail.com>
2020-08-31 22:27:53 -0400

    first commit

---
+++ demo1.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+greeting = 'heey'
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'User {escape(username)}'
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return f'post  {post_id}'
+
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the sabbath after /path/
+    return 'Sabpath %s' % escape(subpath)
+
+
+@app.route('/')
+def index():
+    return '<H1><b><i>index !!!!!!</i></b></H1>'
+
+
+@app.route('/' + greeting)
+def hello():
+    return '<H1><b><i>Hello, World !!!!!!</i></b></H1>'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
